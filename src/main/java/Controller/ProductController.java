3f07b8c17558979fb9db3aa9b4643bdd3dca75e7
gleb_fert <liant379@gmail.com>
2025-01-13 04:26:43 +0300

    feather 63 delete LinkedHashMap, add Files add products.txt

--- src/main/java/Controller/ProductController.java
+++ src/main/java/Controller/ProductController.java
@@ -23,6 +23,7 @@ public class ProductController {
     public void start() throws InputMismatchException {
         String name;
         String typeOfProduct;
+        String input;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -31,47 +32,63 @@ public class ProductController {
             System.out.println("2. Показать все товары");
             System.out.println("3. Найти товар по ID");
             System.out.println("4. Назад");
-            Integer input = scanner.nextInt();
+            input = scanner.next();
 
             switch (input) {
-                case 1 -> {
+                case "1" -> {
                     System.out.println("Введите название товара: ");
                     name = scanner.nextLine();
                     if (name.isBlank()) {
                         name = scanner.nextLine();
                         while (name.replaceAll(NAME_REGEX, "").isBlank()) {
+                            System.out.println(new InputMismatchException("Неверный ввод"));
                             System.out.println("Введите правильное имя товара: ");
                             name = scanner.nextLine();
                         }
                     }
 
-
                     System.out.println("Выберите товар: FOOD/ELECTRONICS/CLOTHING");
                     System.out.println("FOOD - Продукты");
                     System.out.println("ELECTRONICS - Электроника");
                     System.out.println("CLOTHING - Одежда");
-                    typeOfProduct = scanner.nextLine();
+                    typeOfProduct = scanner.nextLine().toUpperCase();
+
                     switch (typeOfProduct) {
-                        case "FOOD" -> productService.addProduct(name, ProductType.FOOD, Integer.parseInt(scanner.nextLine()));
-                        case "ELECTRONICS" -> productService.addProduct(name, ProductType.ELECTRONICS, Integer.parseInt(scanner.nextLine()));
-                        case "CLOTHING" -> productService.addProduct(name, ProductType.CLOTHING, Integer.parseInt(scanner.nextLine()));
+                        case "FOOD" -> {
+                            System.out.println("Введите цену за еденицу товара: ");
+                            Double price = Double.valueOf(scanner.next());
+                            productService.addProduct(name, ProductType.FOOD, price);
+                        }
+                        case "ELECTRONICS" -> {
+                            System.out.println("Введите цену за еденицу товара: ");
+                            Double price = Double.valueOf(scanner.next());
+                            productService.addProduct(name, ProductType.ELECTRONICS, price);
+                        }
+                        case "CLOTHING" -> {
+                            System.out.println("Введите цену за еденицу товара: ");
+                            Double price = Double.valueOf(scanner.next());
+                            productService.addProduct(name, ProductType.CLOTHING, price);
+                        }
                         default -> {
-                            while (!(typeOfProduct.equals("FOOD") || typeOfProduct.equals("ELECTRONICS") || typeOfProduct.equals("CLOTHING"))) {
+                            while (!(typeOfProduct.equalsIgnoreCase("FOOD") || typeOfProduct.equalsIgnoreCase(
+                                    "ELECTRONICS") || typeOfProduct.equalsIgnoreCase("CLOTHING"))) {
                                 System.out.println(new InputMismatchException("Неверный ввод"));
-                                typeOfProduct = scanner.nextLine();
+                                typeOfProduct = scanner.nextLine().toUpperCase();
                             }
-                            productService.addProduct(name, ProductType.valueOf(typeOfProduct), Integer.parseInt(scanner.nextLine()));
+                            System.out.println("Введите цену за еденицу товара: ");
+                            Double price = Double.valueOf(scanner.next());
+                            productService.addProduct(name, ProductType.valueOf(typeOfProduct), price);
                         }
                     }
                 }
-                case 2 -> productService.getAllProducts();
-                case 3 -> {
+                case "2" -> productService.getAllProducts();
+                case "3" -> {
                     System.out.println("Введите ID товара: ");
                     Integer inputID = scanner.nextInt();
                     productService.getProductById(inputID);
                 }
-                case 4 -> System.exit(0);
-                default -> System.out.println(new InputMismatchException().getMessage());
+                case "4" -> start();
+                default -> System.out.println(new InputMismatchException("Неверный ввод"));
             }
         }
     }
--- src/main/java/Repository/OrderRepository.java
+++ src/main/java/Repository/OrderRepository.java
@@ -1,4 +1,46 @@
 package Repository;
 
+import Model.Customer.Customer;
+import Model.Order.Order;
+import Model.Order.OrderStatus;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class OrderRepository {
+    private Order order;
+    private Customer customer;
+    private Path path;
+    private String filePath = "C:\\Users\\User\\IdeaProjects\\Team-Project-\\files\\orders.txt";
+
+    public OrderRepository(Path path) {
+        path = Path.of(filePath);
+    }
+
+    public Order create(Long id, List<Integer> idList) {
+//        order = new Order(id, OrderStatus.NEW /*, idList*/); //TODO принимать List с ID товаров
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+            Files.write(path, (order + "\n").getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public List toList(Path path) {
+        try {
+            return Files.readAllLines(path).stream()
+//                    .map(Customer::new) //TODO переобразовывать файл с продуктами в List
+//
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
